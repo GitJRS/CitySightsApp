dc1d20cdae30bb037f5b13c8709a1fe007c9bc53
gitspammed <xxxspammalotxxx@gmail.com>
2024-04-07 19:52:51 -0400

    - implement new feature

--- CitySights App/ContentView.swift
+++ CitySights App/ContentView.swift
@@ -8,17 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, stranger!")
-        }
-        .padding()
+  var body: some View {
+    VStack {
+      
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundStyle(.tint)
+      Text("Hello, stranger!")
+      
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundStyle(.tint)
+      Text("Hello, stranger!")
+      
     }
+    .padding()
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
